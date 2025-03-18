d4cda654a44a654a8ec6a014dafa5f9fbb970e0c
Alexander  von der Bey <alexandervonderbey@gmail.com>
2025-03-18 13:18:10 +0100

    added db context & mock up sign in for further refinement

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+import { useUser } from "../context/UserContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useUser();
+
+  // Redirection if no user is signed in
+  return user ? children : <Navigate to="/signin" replace />;
+};
+
+export default ProtectedRoute;
