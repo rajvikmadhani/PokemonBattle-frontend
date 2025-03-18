17d807af9ec5f9eb1206d71a98c70934083d66ca
Anne Martinez <martinezannemerlyn@gmail.com>
2025-03-18 10:08:32 +0100

    added structure

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,8 +4,6 @@ const Filter = () => {
             <input
                 type="text"
                 placeholder="Search user..."
-                value={filter}
-                onChange={(e) => setFilter(e.target.value)}
                 className="input input-bordered w-full bg-[#2C2C2C] text-white border-none focus:outline-none focus:ring-2 focus:ring-pink-500 transition duration-300"
             />
         </div>
--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,10 +1,16 @@
-import Filter from "daisyui/components/filter";
+import Filter from "../components/Filter";
 import LeaderboardList from "../components/LeaderboardList";
 
 const Leaderboard = () => {
     return (
-        <div>
-            <LeaderboardList />
+        <div className="min-h-screen bg-[#1F1F1F] text-white py-10">
+            <div className="max-w-4xl mx-auto p-6">
+                <h1 className="text-5xl font-bold text-center text-pink-500 mb-8">
+                    Game Leaderboard
+                </h1>
+                <Filter />
+                <LeaderboardList />
+            </div>
         </div>
     );
 };
