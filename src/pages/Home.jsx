03a4cd67b092f52f70e8ebe53ce1c8ef0b907faa
RajviMadhani <127208465+RajviMadhani@users.noreply.github.com>
2025-07-28 10:58:56 +0200

    Fix import path for Card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { getAllPokemon } from '../utils/pokemonAPI';
-import Card from '../components/Card';
+import Card from '../components/card';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const Home = ({ isMusicOn, searchTerm }) => {
